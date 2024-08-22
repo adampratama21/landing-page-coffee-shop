166a1b9ea5d59af02d05a749292fe2498d827932
Adam Pratama <adam629pratama@gmail.com>
2024-08-22 16:47:04 +0700

    Memidahkan file-file landing page dari folder lama ke folder baru (repo pada Github)

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+alert(
+  "Website ini bersifat fiktif, dengan tujuan untuk melatih kemampuan dalam front-end web development"
+);
+
+const backToTopButton = document.getElementById("back-to-top");
+const header = document.getElementById("nav");
+
+window.onscroll = function () {
+  const headerHeight = header.offsetHeight;
+
+  if (document.documentElement.scrollTop > headerHeight) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
